757de14126618ebecf603d0acf574f5bf4b0910c
Bartozo <bartozo.krol@gmail.com>
2022-09-17 10:13:16 +0200

    :fire: Removed unused imports

--- app/src/main/java/com/bartozo/lifeprogress/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/bartozo/lifeprogress/ui/screens/ProfileScreen.kt
@@ -9,14 +9,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Cake
 import androidx.compose.material.icons.outlined.Face
-import androidx.compose.material.icons.rounded.Cake
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bartozo.lifeprogress.R
