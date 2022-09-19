bcb4c8e303947e7520cd55b58a7a1423a647f29d
Bartozo <bartozo.krol@gmail.com>
2022-09-19 19:13:57 +0200

    :heavy_plus_sign: Added glance

--- app/src/main/java/com/bartozo/lifeprogress/ui/theme/Theme.kt
+++ app/src/main/java/com/bartozo/lifeprogress/ui/theme/Theme.kt
@@ -1,21 +1,15 @@
 package com.bartozo.lifeprogress.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.android.material.color.MaterialColors
+
 
 private val lightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
@@ -93,19 +87,14 @@ fun LifeProgressTheme(
         darkTheme -> darkColorScheme
         else -> lightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
+
+//    val harmonizedColor = MaterialColors.harmonizeWithPrimary(context, colorToHarmonize)
 
     val systemUiController = rememberSystemUiController()
     SideEffect {
