46542ea035c8323252a59fd1fd3b9d6b8195b23f
Bartozo <bartozo.krol@gmail.com>
2022-09-23 12:48:52 +0200

    :sparkles: User can now pin widget to home screen from the profile screen

--- app/src/main/java/com/bartozo/lifeprogress/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/bartozo/lifeprogress/ui/screens/ProfileScreen.kt
@@ -1,6 +1,12 @@
 package com.bartozo.lifeprogress.ui.screens
 
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProviderInfo
+import android.content.Context
+import android.content.Intent
 import androidx.compose.animation.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -12,16 +18,17 @@ import androidx.compose.material.icons.outlined.Cake
 import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bartozo.lifeprogress.R
-import com.bartozo.lifeprogress.ui.components.Header
-import com.bartozo.lifeprogress.ui.components.WorkInProgressCard
+import com.bartozo.lifeprogress.data.Life
+import com.bartozo.lifeprogress.ui.appwidgets.AppWidgetPinnedReceiver
+import com.bartozo.lifeprogress.ui.components.*
 import com.bartozo.lifeprogress.ui.theme.LifeProgressTheme
 import com.bartozo.lifeprogress.ui.viewmodels.ProfileViewModel
 import com.bartozo.lifeprogress.util.rangeOfYearsFromNowTo
@@ -42,8 +49,14 @@ fun ProfileScreen(
         .collectAsState(initial = LocalDate.now())
     val lifeExpectancy: Int? by viewModel.lifeExpectancy
         .collectAsState(initial = 30)
+
     val scrollState = rememberScrollState()
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    val context = LocalContext.current
+    val widgetManager = AppWidgetManager.getInstance(context)
+    // Get a list of our app widget providers to retrieve their info
+    val widgetProviders = widgetManager.getInstalledProvidersForPackage(context.packageName, null)
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -82,6 +95,22 @@ fun ProfileScreen(
                     text = "Themes"
                 )
                 WorkInProgressCard()
+                Divider(
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                )
+                Header(
+                    modifier = Modifier.padding(top = 16.dp, start = 16.dp),
+                    text = "App widgets"
+                )
+                AppWidgetCard(
+                    modifier =  Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
+                    isRequestPinAppWidgetSupported = widgetManager.isRequestPinAppWidgetSupported,
+                    onPinAppWidgetClick = {
+                        // The application has only one widget
+                        widgetProviders.first().pin(context)
+                    }
+                )
             }
         }
     )
@@ -258,6 +287,71 @@ private fun LifeExpectancyCard(
     )
 }
 
+@Composable
+private fun AppWidgetCard(
+    modifier: Modifier = Modifier,
+    isRequestPinAppWidgetSupported: Boolean,
+    onPinAppWidgetClick: () -> Unit
+) {
+
+    Column(modifier = modifier) {
+        if (!isRequestPinAppWidgetSupported) {
+            Text(
+                text = stringResource(id = R.string.pin_unavailable),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.error)
+                    .padding(16.dp),
+                color = MaterialTheme.colorScheme.onError
+            )
+        }
+        InformationCard(
+            modifier = Modifier.padding(top = 16.dp),
+            headline = "Simplified life calendar",
+            supportingText = "See the current life progress with this helpful widget " +
+                    "on your home screen.",
+            onClick = onPinAppWidgetClick,
+            header = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp, vertical = 16.dp)
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    Box(modifier = Modifier.weight(1f)) {
+                        SimplifiedLifeCalendar(
+                            modifier = Modifier
+                                .align(alignment = Alignment.BottomCenter)
+                                .offset(x = 50.dp, y = 50.dp),
+                            life = Life.example
+                        )
+                    }
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
+        )
+    }
+}
+
+/**
+ * Extension method to request the launcher to pin the given AppWidgetProviderInfo
+ *
+ * Note: the optional success callback to retrieve if the widget was placed might be unreliable
+ * depending on the default launcher implementation. Also, it does not callback if user cancels the
+ * request.
+ */
+private fun AppWidgetProviderInfo.pin(context: Context) {
+    val successCallback = PendingIntent.getBroadcast(
+        context,
+        0,
+        Intent(context, AppWidgetPinnedReceiver::class.java),
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+
+    AppWidgetManager.getInstance(context).requestPinAppWidget(provider, null, successCallback)
+}
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
@@ -290,4 +384,15 @@ private fun LifeExpectancyCardPreview() {
            onLifeExpectancySelect = {}
        )
    }
+}
+
+@Preview
+@Composable
+fun AppWidgetCardPreview() {
+    LifeProgressTheme {
+        AppWidgetCard(
+            isRequestPinAppWidgetSupported = false,
+            onPinAppWidgetClick = {}
+        )
+    }
 }
