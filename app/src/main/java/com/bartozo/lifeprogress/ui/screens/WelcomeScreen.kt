dac3a1ac5b3f81de38cbf28eb839964adcec2f6d
Bartozo <bartozo.krol@gmail.com>
2023-02-11 20:48:35 +0100

    💄 Updated welcome screen

--- app/src/main/java/com/bartozo/lifeprogress/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/bartozo/lifeprogress/ui/screens/WelcomeScreen.kt
@@ -1,225 +1,72 @@
 package com.bartozo.lifeprogress.ui.screens
 
 import androidx.compose.animation.*
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Devices
+import androidx.compose.material.icons.outlined.Palette
+import androidx.compose.material.icons.outlined.Widgets
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.bartozo.lifeprogress.R
-import com.bartozo.lifeprogress.ui.theme.LifeProgressTheme
-import com.bartozo.lifeprogress.ui.viewmodels.WelcomeEventState
-import com.bartozo.lifeprogress.ui.viewmodels.WelcomeViewModel
-import com.bartozo.lifeprogress.util.supportWideScreen
-import kotlinx.coroutines.delay
-import kotlin.time.Duration.Companion.seconds
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ExperimentalTextApi
+import com.bartozo.lifeprogress.ui.components.welcome.Feature
+import com.bartozo.lifeprogress.ui.components.welcome.PrimaryActionColors
+import com.bartozo.lifeprogress.ui.components.welcome.Welcome
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun WelcomeScreen(
-    viewModel: WelcomeViewModel,
     navigateToProfileScreen: () -> Unit,
 ) {
-    val welcomeEvent: WelcomeEventState by viewModel.welcomeEvent
-        .collectAsState()
-
-    LaunchedEffect(key1 = welcomeEvent) {
-        when (welcomeEvent) {
-            WelcomeEventState.Idle -> {}
-            WelcomeEventState.NavigateToProfileScreen -> navigateToProfileScreen()
-        }
-    }
-
-    var isAppIconVisible by remember { mutableStateOf(false) }
-    var isHeadlineVisible by remember { mutableStateOf(false) }
-    var isSecondHeadlineVisible by remember { mutableStateOf(false) }
-    var isDescriptionVisible by remember { mutableStateOf(false) }
-    var isCaptionVisible by remember { mutableStateOf(false) }
-    var isButtonVisible by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        delay(1.seconds)
-        isAppIconVisible = true
-
-        delay(1.seconds)
-        isHeadlineVisible = true
-        isSecondHeadlineVisible = true
-
-        delay(2.seconds)
-        isDescriptionVisible = true
-
-        delay(2.seconds)
-        isCaptionVisible = true
-        isButtonVisible = true
-    }
-
-    Scaffold(
-        content = { innerPaddings ->
-            WelcomeContent(
-                modifier = Modifier
-                    .supportWideScreen()
-                    .fillMaxHeight()
-                    .padding(innerPaddings)
-                    .padding(horizontal = 32.dp),
-                isAppIconVisible = isAppIconVisible,
-                isHeadlineVisible = isHeadlineVisible,
-                isSecondHeadlineVisible = isSecondHeadlineVisible,
-                isDescriptionVisible = isDescriptionVisible
-            )
-        },
-        bottomBar = {
-            WelcomeBottomBar(
-                modifier = Modifier
-                    .supportWideScreen()
-                    .systemBarsPadding()
-                    .padding(horizontal = 32.dp, vertical = 20.dp),
-                isCaptionVisible = isCaptionVisible,
-                isButtonVisible = isButtonVisible,
-                onContinueClick = { viewModel.navigateToProfileScreen() }
-            )
-        },
-    )
-}
-
-@Composable
-private fun WelcomeContent(
-    modifier: Modifier = Modifier,
-    isAppIconVisible: Boolean,
-    isHeadlineVisible: Boolean,
-    isSecondHeadlineVisible: Boolean,
-    isDescriptionVisible: Boolean
-) {
-    val scrollState = rememberScrollState()
-
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .verticalScroll(scrollState),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.Start
-    ) {
-        AppearAnimation(isVisible = isAppIconVisible) {
-            Image(
-                modifier = Modifier
-                    .size(120.dp)
-                    .padding(bottom = 24.dp),
-                painter = painterResource(id = R.drawable.life_progress_app_icon),
-                contentDescription = "App icon"
-            )
-        }
-        Column(modifier = Modifier.padding(bottom = 16.dp)) {
-            AppearAnimation(isVisible = isHeadlineVisible) {
-                Text(
-                    stringResource(id = R.string.welcome_to),
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = MaterialTheme.colorScheme.onSurface
+    Welcome(
+        title = "Welcome to",
+        titleTextStyle = MaterialTheme.typography.displaySmall,
+        secondaryTitle = "Life Progress",
+        secondaryTextStyle = MaterialTheme.typography.displaySmall.copy(
+            brush  = Brush.linearGradient(
+                colors = listOf(
+                    Color(0xFF069DDE),
+                    Color(0xFF5FB945),
+                    Color(0xFFF9B828),
+                    Color(0xFFF7801A),
+                    Color(0xFFDF3B3C),
+                    Color(0xFF943B95),
+                    Color(0xFF955F3B)
                 )
-            }
-            AppearAnimation(isVisible = isSecondHeadlineVisible) {
-                Text(
-                    stringResource(id = R.string.app_name),
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-            }
-        }
-        AppearAnimation(isVisible = isDescriptionVisible) {
-            Text(
-                stringResource(id = R.string.friendly_reminder_that),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                textAlign = TextAlign.Start
-            )
-        }
-    }
-}
-
-@Composable
-private fun WelcomeBottomBar(
-    modifier: Modifier = Modifier,
-    isCaptionVisible: Boolean,
-    isButtonVisible: Boolean,
-    onContinueClick: () -> Unit
-) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        AppearAnimation(isVisible = isCaptionVisible) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = stringResource(id = R.string.before_we_continue),
-                style = MaterialTheme.typography.bodySmall,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                textAlign = TextAlign.Center
-            )
-        }
-        AppearAnimation(isVisible = isButtonVisible) {
-            FilledTonalButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(min = 48.dp),
-                onClick = onContinueClick
-            ) {
-                Text(
-                    text = stringResource(id = R.string.continue_button_text),
-                    style = MaterialTheme.typography.labelLarge
-                )
-            }
-        }
-    }
-}
-
-@Composable
-private fun AppearAnimation(
-    isVisible: Boolean,
-    content: @Composable () -> Unit
-) {
-    AnimatedVisibility(
-        visible = isVisible,
-        enter = slideInVertically() + expandVertically(
-            // Expand from the top.
-            expandFrom = Alignment.Top
-        ) + fadeIn(
-            // Fade in with the initial alpha of 0.3f.
-            initialAlpha = 0.3f
+            ),
         ),
-    ) {
-        content()
-    }
-}
-
-@Preview
-@Composable
-private fun WelcomeContentPreview() {
-    LifeProgressTheme {
-        WelcomeContent(
-            isAppIconVisible = true,
-            isHeadlineVisible = true,
-            isSecondHeadlineVisible = true,
-            isDescriptionVisible = true
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun WelcomeBottomBarPreview() {
-    LifeProgressTheme {
-        WelcomeBottomBar(
-            isCaptionVisible = true,
-            isButtonVisible = true,
-            onContinueClick = {}
-        )
-    }
+        features = listOf(
+            Feature(
+                title = "Measure your Life Progress",
+                subtitle = "See the progress of your life and current year with easy to use calendar.",
+                image = Icons.Outlined.DateRange
+            ),
+            Feature(
+                title = "Widget",
+                subtitle = "Always be up to date with your life progress with super handy " +
+                        "and useful widget on your home screen.",
+                image = Icons.Outlined.Widgets
+            ),
+            Feature(
+                title = "App Theme",
+                subtitle = "Material You (The application color palette is generated based on " +
+                        "the user's wallpaper, only available for android 12 or higher).",
+                image = Icons.Outlined.Palette
+            ),
+            Feature(
+                title = "Different Modes",
+                subtitle = "Works in both Horizontal and Vertical mode.",
+                image = Icons.Outlined.Devices
+            ),
+        ),
+        primaryActionColors = PrimaryActionColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        onPrimaryActionClicked = navigateToProfileScreen,
+    )
 }
